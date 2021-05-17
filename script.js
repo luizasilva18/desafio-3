6b606c5164437542319c7b671ca84dd6c13cfa59
luizasilva18clear <luizagoncalves7@gmail.com>
2021-05-16 22:11:48 -0300

    Desafio numero 3

---
+++ script.js
@@ -0,0 +1,19 @@
+for(let i = 1 ; i <= 10 ; i ++){
+    console.log (i)
+}
+
+
+var r = 10;
+while(r >= 1){
+    console.log(r)
+    r --
+
+}
+
+for(let a = 1 ; a <= 100 ; a +=2){
+    console.log (a)
+}
+
+for(let b = 0 ; b <= 100 ; b +=2){
+    console.log (b)
+}
